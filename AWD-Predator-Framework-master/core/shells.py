32ae82c3a4c21417384634763827ad9aa60416a8
Chromos2me <153702831+Chromos2me@users.noreply.github.com>
2024-04-13 17:43:54 +0800

    Update shells.py

--- AWD-Predator-Framework-master/core/shells.py
+++ AWD-Predator-Framework-master/core/shells.py
@@ -1,73 +1,85 @@
 # -*- coding: utf-8 -*-
 import pickle
 
-# Example:GET_eval_shell_paths_pwd = {"/eval.php":"pass"}
-
+# 导入 core.ip_list 模块
 from core.ip_list import *
 
-#GET_eval_shells_path_pwd,POST_eval_shells_path_pwd,GET_exec_shells_path_pwd,POST_exec_shells_path_pwd={},{},{},{}
+# 定义全局变量用于存储shell路径和密码
+GET_eval_shells_path_pwd = {}
+POST_eval_shells_path_pwd = {}
+GET_exec_shells_path_pwd = {}
+POST_exec_shells_path_pwd = {}
 
 
-def GET_eval_shell_path_pwd(path,pwd):
+# 添加GET eval shell路径和密码
+def GET_eval_shell_path_pwd(path, pwd):
     global GET_eval_shells_path_pwd
     GET_eval_shells_path_pwd[path] = pwd
 
 
-def POST_eval_shell_path_pwd(path,pwd):
+# 添加POST eval shell路径和密码
+def POST_eval_shell_path_pwd(path, pwd):
     global POST_eval_shells_path_pwd
     POST_eval_shells_path_pwd[path] = pwd
 
 
-def GET_exec_shell_path_pwd(path,pwd):
+# 添加GET exec shell路径和密码
+def GET_exec_shell_path_pwd(path, pwd):
     global GET_exec_shells_path_pwd
     GET_exec_shells_path_pwd[path] = pwd
 
 
-def POST_exec_shell_path_pwd(path,pwd):
+# 添加POST exec shell路径和密码
+def POST_exec_shell_path_pwd(path, pwd):
     global POST_exec_shells_path_pwd
     POST_exec_shells_path_pwd[path] = pwd
 
 
+# 获取GET eval shell路径和密码
 def Get_GET_eval_sap():
     return GET_eval_shells_path_pwd
 
 
+# 获取POST eval shell路径和密码
 def Get_Post_eval_sap():
     return POST_eval_shells_path_pwd
 
 
+# 获取GET exec shell路径和密码
 def Get_GET_exec_sap():
     return GET_exec_shells_path_pwd
 
 
+# 获取POST exec shell路径和密码
 def Get_POST_exec_sap():
     return POST_exec_shells_path_pwd
 
 
+# 显示所有shell路径和密码
 def show_sap():
     for i in GET_eval_shells_path_pwd:
-        print (i + ':' + GET_eval_shells_path_pwd[i] + ' eval get')
+        print(i + ':' + GET_eval_shells_path_pwd[i] + ' eval get')
     for i in POST_eval_shells_path_pwd:
-        print (i + ':' + POST_eval_shells_path_pwd[i] + ' eval post')
+        print(i + ':' + POST_eval_shells_path_pwd[i] + ' eval post')
     for i in GET_exec_shells_path_pwd:
-        print (i + ':' + GET_exec_shells_path_pwd[i] + ' exec get')
+        print(i + ':' + GET_exec_shells_path_pwd[i] + ' exec get')
     for i in POST_exec_shells_path_pwd:
-        print (i + ':' + POST_exec_shells_path_pwd[i] + ' exec post')
+        print(i + ':' + POST_exec_shells_path_pwd[i] + ' exec post')
 
 
 # 保存shell路径和密码
 def save_shell_path_pwd():
-    with open('data/GET_eval.pickle','wb') as f:
-        pickle.dump(GET_eval_shells_path_pwd,f)
-    with open('data/POST_eval.pickle','wb') as y:
-        pickle.dump(POST_eval_shells_path_pwd,y)
-    with open('data/GET_exec.pickle','wb') as g:
-        pickle.dump(GET_exec_shells_path_pwd,g)
-    with open('data/POST_exec.pickle','wb') as u:
-        pickle.dump(POST_exec_shells_path_pwd,u)
-    target = open('auxi\webshell.txt','w')
+    with open('data/GET_eval.pickle', 'wb') as f:
+        pickle.dump(GET_eval_shells_path_pwd, f)
+    with open('data/POST_eval.pickle', 'wb') as y:
+        pickle.dump(POST_eval_shells_path_pwd, y)
+    with open('data/GET_exec.pickle', 'wb') as g:
+        pickle.dump(GET_exec_shells_path_pwd, g)
+    with open('data/POST_exec.pickle', 'wb') as u:
+        pickle.dump(POST_exec_shells_path_pwd, u)
+    target = open('auxi/webshell.txt', 'w')
     for i in ipList:
-        print (i)
+        print(i)
         for j in POST_eval_shells_path_pwd:
             target.write('http://')
             target.write(i)
@@ -107,35 +119,147 @@ def save_shell_path_pwd():
             target.write('\n')
         """
     target.close()
-    print ("save ok")
+    print("save ok")
 
 
 # 读取shell路径和密码
 def load_shell_path_pwd():
-    global GET_eval_shells_path_pwd,POST_eval_shells_path_pwd,GET_exec_shells_path_pwd,POST_exec_shells_path_pwd
-    with open('data/GET_eval.pickle','r') as f:
+    global GET_eval_shells_path_pwd, POST_eval_shells_path_pwd, GET_exec_shells_path_pwd, POST_exec_shells_path_pwd
+    with open('data/GET_eval.pickle', 'rb') as f:
         a = pickle.load(f)
-        GET_eval_shells_path_pwd=a
-    with open('data/POST_eval.pickle','r') as y:
-         b= pickle.load(y)
-         POST_eval_shells_path_pwd=b
-    with open('data/GET_exec.pickle','r') as g:
-         c= pickle.load(g)
-         GET_exec_shells_path_pwd=c
-    with open('data/POST_exec.pickle','r') as u:
-         d= pickle.load(u)
-         POST_exec_shells_path_pwd=d
-    print ("load ok")
+        GET_eval_shells_path_pwd = a
+    with open('data/POST_eval.pickle', 'rb') as y:
+        b = pickle.load(y)
+        POST_eval_shells_path_pwd =在给定的代码中，有一些错误和问题。下面是修复后的代码，并添加了注释：
+
+```python
+# -*- coding: utf-8 -*-
+import pickle
+
+# 导入 core.ip_list 模块
+from core.ip_list import *
+
+# 定义全局变量用于存储 shell 路径和密码
+GET_eval_shells_path_pwd = {}
+POST_eval_shells_path_pwd = {}
+GET_exec_shells_path_pwd = {}
+POST_exec_shells_path_pwd = {}
 
 
-# 清除shell路径和密码
-def clear_shell_path_pwd():
-    global GET_eval_shells_path_pwd,POST_eval_shells_path_pwd,GET_exec_shells_path_pwd,POST_exec_shells_path_pwd
-    GET_eval_shells_path_pwd.clear()
-    POST_eval_shells_path_pwd.clear()
-    GET_exec_shells_path_pwd.clear()
-    POST_exec_shells_path_pwd.clear()
-    print ("clear ok")
+# 添加 GET eval shell 路径和密码
+def GET_eval_shell_path_pwd(path, pwd):
+    global GET_eval_shells_path_pwd
+    GET_eval_shells_path_pwd[path] = pwd
 
 
+# 添加 POST eval shell 路径和密码
+def POST_eval_shell_path_pwd(path, pwd):
+    global POST_eval_shells_path_pwd
+    POST_eval_shells_path_pwd[path] = pwd
 
+
+# 添加 GET exec shell 路径和密码
+def GET_exec_shell_path_pwd(path, pwd):
+    global GET_exec_shells_path_pwd
+    GET_exec_shells_path_pwd[path] = pwd
+
+
+# 添加 POST exec shell 路径和密码
+def POST_exec_shell_path_pwd(path, pwd):
+    global POST_exec_shells_path_pwd
+    POST_exec_shells_path_pwd[path] = pwd
+
+
+# 获取 GET eval shell 路径和密码
+def Get_GET_eval_sap():
+    return GET_eval_shells_path_pwd
+
+
+# 获取 POST eval shell 路径和密码
+def Get_Post_eval_sap():
+    return POST_eval_shells_path_pwd
+
+
+# 获取 GET exec shell 路径和密码
+def Get_GET_exec_sap():
+    return GET_exec_shells_path_pwd
+
+
+# 获取 POST exec shell 路径和密码
+def Get_POST_exec_sap():
+    return POST_exec_shells_path_pwd
+
+
+# 显示所有 shell 路径和密码
+def show_sap():
+    for i in GET_eval_shells_path_pwd:
+        print(i + ':' + GET_eval_shells_path_pwd[i] + ' eval get')
+    for i in POST_eval_shells_path_pwd:
+        print(i + ':' + POST_eval_shells_path_pwd[i] + ' eval post')
+    for i in GET_exec_shells_path_pwd:
+        print(i + ':' + GET_exec_shells_path_pwd[i] + ' exec get')
+    for i in POST_exec_shells_path_pwd:
+        print(i + ':' + POST_exec_shells_path_pwd[i] + ' exec post')
+
+
+# 保存 shell 路径和密码
+def save_shell_path_pwd():
+    with open('data/GET_eval.pickle', 'wb') as f:
+        pickle.dump(GET_eval_shells_path_pwd, f)
+    with open('data/POST_eval.pickle', 'wb') as y:
+        pickle.dump(POST_eval_shells_path_pwd, y)
+    with open('data/GET_exec.pickle', 'wb') as g:
+        pickle.dump(GET_exec_shells_path_pwd, g)
+    with open('data/POST_exec.pickle', 'wb') as u:
+        pickle.dump(POST_exec_shells_path_pwd, u)
+    target = open('auxi/webshell.txt', 'w')
+    for i in ipList:
+        print(i)
+        for j in POST_eval_shells_path_pwd:
+            target.write('http://')
+            target.write(i)
+            target.write(j)
+            target.write(',')
+            target.write('post')
+            target.write(',')
+            target.write(POST_eval_shells_path_pwd[j])
+            target.write('\n')
+        for k in GET_eval_shells_path_pwd:
+            target.write('http://')
+            target.write(i)
+            target.write(k)
+            target.write(',')
+            target.write('get')
+            target.write(',')
+            target.write(GET_eval_shells_path_pwd[k])
+            target.write('\n')
+        """
+        for m in GET_exec_shells_path_pwd:
+            target.write('http://')
+            target.write(i)
+            target.write(m)
+            target.write(',')
+            target.write('get')
+            target.write(',')
+            target.write(GET_exec_shells_path_pwd[m])
+            target.write('\n')
+        for n in POST_exec_shells_path_pwd:
+            target.write('http://')
+            target.write(i)
+            target.write(n)
+            target.write(',')
+            target.write('get')
+            target.write(',')
+            target.write(POST_exec_shells_path_pwd[n])
+            target.write('\n')
+        """
+    target.close()
+    print("save ok")
+
+
+# 读取 shell 路径和密码
+def load_shell_path_pwd():
+    global GET_eval_shells_path_pwd, POST_eval_shells_path_pwd, GET_exec_shells_path_pwd, POST_exec_shells_path_pwd
+    with open('data/GET_eval.pickle', 'rb') as f:
+        GET_eval_shells_path_pwd = pickle.load(f)
+    with open('data/
